480a00ab6bdd64bc80a1742ad2c4813aaeebeed7
akbararyo <akbararyo@gmail.com>
2019-09-26 11:11:54 +0700

    Add files via upload

---
+++ CalculatorApp/CalculatorWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CalculatorWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nilai A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai B";
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(113, 42);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(100, 20);
+            this.txtNilaiA.TabIndex = 2;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(113, 86);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(100, 20);
+            this.txtNilaiB.TabIndex = 3;
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(113, 127);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(100, 38);
+            this.btnHitung.TabIndex = 4;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstHasil);
+            this.groupBox1.Location = new System.Drawing.Point(40, 181);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(600, 257);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Hasil ]";
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.Location = new System.Drawing.Point(6, 19);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(588, 225);
+            this.lstHasil.TabIndex = 0;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Penjumlahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.comboBox.Location = new System.Drawing.Point(385, 42);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 6;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Operasi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hasil";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(386, 86);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 30);
+            this.listBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Hasil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Calculator Versi Windows";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstHasil;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
